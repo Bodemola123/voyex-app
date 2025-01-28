33ce82d75aef3688632fe4883dcd7466003cc3ea
lawrenceokim <lawrence4okim@gmail.com>
2025-01-29 00:22:08 +0100

    minor 3

--- app/StoreProvider.js
+++ app/StoreProvider.js
@@ -6,8 +6,6 @@ import { makeStore } from "../lib/store";
 import axios from "axios";
 import OrgSignupPopup from "@/components/organization-signing-page/OrgSignupPopup";
 import { Flip, Slide, toast, ToastContainer } from "react-toastify";
-import UserUploadDetails from "@/components/user-signing-page/UserUploadDetails";
-// import { updateOrgOverlay } from "@/lib/features/authentication/auth";
 
 export default function StoreProvider({ children }) {
   // const { orgOverlay } = useSelector((state) => state.auth);
@@ -56,38 +54,6 @@ export default function StoreProvider({ children }) {
       uploadOrgInfoOverlay();
     }
   }, []);
-
-  ///////////// CHECK USER CREDENTIALS //////////////////////
-  // useEffect(() => {
-  //   const userId = localStorage.getItem("userId")
-  //     ? JSON.parse(localStorage.getItem("userId"))
-  //     : null;
-
-  //   if (!userId) {
-  //     return;
-  //   } else {
-  //     const uploadUserInfoOverlay = async () => {
-  //       try {
-  //         const response = await axios.get(
-  //           `https://ek251cvxyd.execute-api.eu-north-1.amazonaws.com/default/users_voyex?user_id=${userId}`
-  //         );
-  //         console.log("checked userid response🚨:", response);
-  //         // if credentials not complete, display form
-  //         if (response.status === 200 && response.data?.fullname === null) {
-  //           setUserDisplay(true);
-  //         }
-  //         if (response.status === 200 && response.data?.fullname !== null) {
-  //           setUserDisplay(false);
-  //         }
-  //       } catch (error) {
-  //         if (error.response?.data) {
-  //           // toast.error(error.response.data);
-  //         } else toast.error(error.message);
-  //       }
-  //     };
-  //     uploadUserInfoOverlay();
-  //   }
-  // }, []);
 
   return (
     <Provider store={storeRef.current}>
