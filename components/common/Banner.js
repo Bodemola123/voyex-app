e13096b0627978975de0511ccb87a7f4d8ca929d
Bodemola <bodemola123@gmail.com>
2025-05-28 12:31:19 +0100

    banner updated

--- components/common/Banner.js
+++ components/common/Banner.js
@@ -6,10 +6,7 @@ const Banner = () => {
   return (
     <div
     id='banner'
-    className="flex items-center justify-center gap-6 py-2 z-50 w-full rounded-xl"
-    style={{
-      background: 'linear-gradient(90deg, #C088FB 3.3%, #FFFFFF 51.15%, #FFFFFF 75.38%, #C088FB 100%)'
-    }}>
+    className="flex items-center justify-center gap-6 py-2 z-50 w-full rounded-xl bg-[#ffffff]">
     <p className='text-[#0a0a0b] font-medium text-base'>Are you a product owner looking to gain Publicity on your product?</p>
     <Link href="/auth/organization" id='banner_button' className='bg-[#C088FB33] flex justify-center items-center text-center text-[#c088fb] font-bold text-base py-3 px-4 rounded-2xl'>
   <p>Sign Up as Organization</p>
